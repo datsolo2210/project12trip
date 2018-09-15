8893c03dbd5b63a92ca557b54a2ecae17122252d
zero2603 <20153713@student.hust.edu.vn>
2018-09-15 10:19:41 +0700

    fix css

--- src/components/quickReview/formSubmit.js
+++ src/components/quickReview/formSubmit.js
@@ -17,27 +17,46 @@ class FormSubmit extends Component {
                 </div>
                 <div style={{ clear: 'both' }} className=" form_review" id="form-content">
                     <form id="submit">
-                        <span className="title_review">Title of your review</span><br />
-                        <input type="text" name="title_review" placeholder="Summarize your visit or hightlight an interesting detail" className="submit_review" /><br />
-                        <span className="title_review">Your review</span><br />
-                        <textarea name="your_review" rows={4} cols={100} form="submit" placeholder="By sharing your experiences, you are helping traveles make better choices and plan their dream trips. Thanks you!" className="your_review" defaultValue={""} /><br />
-                        <span className="title_review">When did you travel?</span><br />
-                        <select>
-                            <option value="Select one">Select one</option>
-                            <option value="January">January</option>
-                            <option value="February">February</option>
-                            <option value="March">March</option>
-                            <option value="April">April</option>
-                            <option value="May">May</option>
-                            <option value="June">June</option>
-                            <option value="July">July</option>
-                            <option value="August">August</option>
-                            <option value="September">September</option>
-                            <option value="October">October</option>
-                            <option value="November">November</option>
-                            <option value="December">December</option>
-                        </select><br />
-                        <input type="checkbox" name="checkbox" className="check_review" /><span className="check_review"> I certify that this review is based on my own experience and is my genuine opinion of this hotel, and that I have no personal or business relationship with this establishment, and have not been offered any incentive or payment originating from the establishment to write this review. I understand that TripAdvisor has a zero-tolerance policy on fake reviews.</span><br />
+                        <div className="form-group">
+                            <label className="title_review">Title of your review</label>
+                            <input type="text" name="title_review" placeholder="Summarize your visit or hightlight an interesting detail" 
+                                className="submit_review form-control" />
+                        </div>
+                        <div className="form-group">
+                        <label className="title_review">Your review</label>
+                        <textarea name="your_review" rows={4} cols={100} form="submit" 
+                            placeholder="By sharing your experiences, you are helping traveles make better choices and plan their dream trips. Thanks you!" 
+                            className="your_review form-control" defaultValue={""} />
+                        </div>
+                        <div className="form-group">
+                            <label className="title_review">When did you travel?</label>
+                            <select className='form-control custom-select-box'>
+                                <option value="">Select one</option>
+                                <option value="January">January</option>
+                                <option value="February">February</option>
+                                <option value="March">March</option>
+                                <option value="April">April</option>
+                                <option value="May">May</option>
+                                <option value="June">June</option>
+                                <option value="July">July</option>
+                                <option value="August">August</option>
+                                <option value="September">September</option>
+                                <option value="October">October</option>
+                                <option value="November">November</option>
+                                <option value="December">December</option>
+                            </select>
+                        </div>    
+                        <div className="form-check">
+                            <input type="checkbox" name="checkbox" className="check_review form-check-input" />
+                            <label class="form-check-label">
+                                <small>
+                                I certify that this review is based on my own experience and is my genuine opinion of this hotel, 
+                                and that I have no personal or business relationship with this establishment, and have not been offered 
+                                any incentive or payment originating from the establishment to write this review. 
+                                I understand that TripAdvisor has a zero-tolerance policy on fake reviews.
+                                </small>
+                            </label>
+                        </div>
                         <input type="submit" name="submit" value="Submit" className="btn_submit" />
                     </form>
                 </div>
