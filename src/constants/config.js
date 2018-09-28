5430e7be58f7e8c026c2c1e0e320fcf6a064e4c9
zero2603 <20153713@student.hust.edu.vn>
2018-09-28 11:55:19 +0700

    fix url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
 export const API_URL = 'http://35.185.188.47/api/';
-export const HOTEL_API = 'http://dev.joombooking.com/12tripapi/';
-// export const HOTEL_API = 'https://app.12trip.vn/';
+// export const HOTEL_API = 'http://dev.joombooking.com/12tripapi/';
+export const HOTEL_API = 'https://app.12trip.vn/';
 export const LOGIN_API = 'http://35.185.188.47/api/login';
